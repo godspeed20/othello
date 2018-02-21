f39cfff278ff12732e39c5c35ed057bc95b7c96a
godspeed20 <rzkd@hotmail.com>
2018-02-21 23:54:24 +0800

    more explicit testing

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 * After every command (including invalid moves) it will re-show the current game board. This may not be the most ideal but it simplifies the code a little and avoids having to scroll back up.
 * To make it easier to comprehend when reading the code, Players are called A and B internally. However, the output uses X and O as required.
 * OthelloGame is properly tested via the full end to end test OthelloAppTest, hence the test file for this class testing one case only.
-* Some level of 'AI' is exposed to test end states to simplify the testing. This can be refactored with more time.
 * Some test cases are missing due time frames. This includes player b winning, there being a tie (assuming this is possible), moves being possible for one but not the other player, etc. With more time these test cases can be identified and added. 
 * With my limited Maven experience, I found every time I built anything it seemed to assumed I was using java 1.5 for some reason so I've added the maven compiler plugin to get around this. It may not be necessary for you to build, feel free to remove as per your situation.
 
--- src/test/java/com/othello/OthelloGameTest.java
+++ src/test/java/com/othello/OthelloGameTest.java
@@ -3,8 +3,6 @@ package com.othello;
 import com.othello.writer.MockWriter;
 import org.junit.Test;
 
-import java.util.Optional;
-
 import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -12,13 +10,67 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class OthelloGameTest {
     @Test
     public void testEndStateFullBoard() {
-        Board board = Board.newBoard();
-
-        Optional<Coordinate> nextMove = board.bestNextMove();
-        while (nextMove.isPresent()) {
-            board = board.validateAndApply(nextMove.get().coordinate());
-            nextMove = board.bestNextMove();
-        }
+        Board board = Board.newBoard()
+                .validateAndApply("e6")
+                .validateAndApply("f6")
+                .validateAndApply("g6")
+                .validateAndApply("g7")
+                .validateAndApply("g8")
+                .validateAndApply("d6")
+                .validateAndApply("c6")
+                .validateAndApply("h8")
+                .validateAndApply("f5")
+                .validateAndApply("b6")
+                .validateAndApply("a6")
+                .validateAndApply("d7")
+                .validateAndApply("c4")
+                .validateAndApply("h6")
+                .validateAndApply("d8")
+                .validateAndApply("e3")
+                .validateAndApply("f7")
+                .validateAndApply("e7")
+                .validateAndApply("h7")
+                .validateAndApply("f8")
+                .validateAndApply("e2")
+                .validateAndApply("b3")
+                .validateAndApply("e8")
+                .validateAndApply("f4")
+                .validateAndApply("d3")
+                .validateAndApply("c7")
+                .validateAndApply("b7")
+                .validateAndApply("d2")
+                .validateAndApply("d1")
+                .validateAndApply("c5")
+                .validateAndApply("c3")
+                .validateAndApply("a7")
+                .validateAndApply("c8")
+                .validateAndApply("b8")
+                .validateAndApply("a8")
+                .validateAndApply("g5")
+                .validateAndApply("f3")
+                .validateAndApply("f2")
+                .validateAndApply("g1")
+                .validateAndApply("c2")
+                .validateAndApply("b1")
+                .validateAndApply("e1")
+                .validateAndApply("b5")
+                .validateAndApply("a5")
+                .validateAndApply("g4")
+                .validateAndApply("f1")
+                .validateAndApply("g2")
+                .validateAndApply("g3")
+                .validateAndApply("b4")
+                .validateAndApply("a4")
+                .validateAndApply("h3")
+                .validateAndApply("c1")
+                .validateAndApply("b2")
+                .validateAndApply("a2")
+                .validateAndApply("a3")
+                .validateAndApply("h1")
+                .validateAndApply("h5")
+                .validateAndApply("h4")
+                .validateAndApply("a1")
+                .validateAndApply("h2");
 
         OthelloGame game = new OthelloGame(board);
 
